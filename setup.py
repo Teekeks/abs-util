d3c4d07a6960512bd74831050ff3637447680a44
Teekeks <info@teawork.de>
2024-09-30 14:37:36 +0200

    feat: adding basic setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = ''
+
+with open('abs_util/__init__.py') as f:
+    for line in f.readlines():
+        if line.startswith('__version__'):
+            version = line.split('= \'')[-1][:-2].strip()
+
+if version.endswith(('a', 'b', 'rc')):
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except:
+        pass
+
+setup(
+    packages=find_packages(),
+    version=version,
+    keywords=['audiobookshelf'],
+    install_requires=[
+        'pick',
+        'audiobookshelf@git+https://github.com/Teekeks/audiobookshelfAPI.git',
+        'colorama~=0.4.6',
+        'prompt_toolkit',
+        'aiosqlite',
+        'beautifulsoup4',
+        'aiohttp'
+    ],
+    package_data={'abs_util': ['py.typed']}
+)
